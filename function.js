f8d9ea8b47d944877fbceb0ec5d5d988ed5106aa
Raed Salah Bzour <raedbzour77@gmail.com>
2023-12-15 22:44:23 +0200

    finishing

---
+++ function.js
@@ -0,0 +1,34 @@
+const students = [
+  { name: "ahmad", marks: [85, 92, 88] },
+  { name: "ali", marks: [78, 85, 90] },
+  { name: "salma", marks: [90, 95, 87] },
+];
+
+const withAverage = students.map(student => {
+  let sum = 0;
+  for (let i = 0; i < student.marks.length; i++) {
+    sum += student.marks[i];
+  }
+  let average = (sum / student.marks.length).toFixed(3);
+  return { ...student, average }; //return the same object with the additional of the average
+});
+console.log(withAverage);
+
+withAverage.filter(student => {
+  if (student.average > 90) {
+    console.log(student.name);
+  }
+});
+
+// const averageOfAveragees = withAverage.reduce((averageClass, current) => {
+//   averageClass += current;
+//   return averageClass / 3;
+// });
+// console.log(averageOfAveragees);
+
+withAverage.forEach((student, index) => {
+  student.marks[2] = 50;
+});
+console.log(withAverage);
+
+console.log(withAverage.find(student => student.name === "ali"));
