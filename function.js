9622fe89895b0ea75129eed27772ff250ef6076e
Raed Salah Bzour <raedbzour77@gmail.com>
2023-12-23 16:45:03 +0200

    finishing

--- function.js
+++ function.js
@@ -3,29 +3,29 @@ const students = [
   { name: "ali", marks: [78, 85, 90] },
   { name: "salma", marks: [90, 95, 87] },
 ];
-
 const withAverage = students.map(student => {
   let sum = 0;
   for (let i = 0; i < student.marks.length; i++) {
     sum += student.marks[i];
   }
-  let average = (sum / student.marks.length).toFixed(3);
+  let average = Math.floor(sum / student.marks.length);
   return { ...student, average }; //return the same object with the additional of the average
 });
 console.log(withAverage);
 
-withAverage.filter(student => {
-  if (student.average > 90) {
-    console.log(student.name);
-  }
-});
+console.log(
+  withAverage.filter(student => {
+    if (student.average > 90) {
+      console.log(student.name);
+    }
+  })
+);
 
-// const averageOfAveragees = withAverage.reduce((averageClass, current) => {
-//   averageClass += current;
-//   return averageClass / 3;
-// });
-// console.log(averageOfAveragees);
-
+const totalAverages = withAverage.reduce(
+  (acc, student) => acc + student.average,
+  0
+);
+console.log(totalAverages / 3);
 withAverage.forEach((student, index) => {
   student.marks[2] = 50;
 });
